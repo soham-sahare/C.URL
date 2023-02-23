1893c9111f847e895131314a83e6e419f6ae6e66
sohamsahare <sohamsahare123@gmail.com>
2023-02-23 09:51:11 +0530

    production build

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ const mongoose = require('mongoose');
 const api = require('./api/routes');
 const path = require('path');
 
-//init express app
 const app = express();
 app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-//connect to mongoDB:
 const { DB_URI } = process.env;
 mongoose
   .connect(DB_URI, {
@@ -30,14 +28,15 @@ mongoose
 app.use('/api', api);
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
+
+  console.log(process.env.NODE_ENV);
+
   app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
+
   app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
-//configure port and run server
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Server running on port ' + port));
---
+++ client/build/precache-manifest.e7d97ff4bbd2372f7b4d0e756939a689.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb0f61835ade1ad99f13db74ea9361b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1a353368e2f799e1f92",
+    "url": "/static/css/2.19e10c4f.chunk.css"
+  },
+  {
+    "revision": "ffe6e3f2d5a5d7e18b66",
+    "url": "/static/css/main.d6db031e.chunk.css"
+  },
+  {
+    "revision": "b1a353368e2f799e1f92",
+    "url": "/static/js/2.4cc8888c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4cc8888c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe6e3f2d5a5d7e18b66",
+    "url": "/static/js/main.2388d7ff.chunk.js"
+  },
+  {
+    "revision": "73b751b8c56bac08c059",
+    "url": "/static/js/runtime-main.6eb3969d.js"
+  }
+]);
